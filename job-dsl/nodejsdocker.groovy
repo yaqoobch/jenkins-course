81320d54bee8780aad7464e18a649ae8399ee3e2
yaqoobch <47523252+yaqoobch@users.noreply.github.com>
2019-12-20 11:58:25 +0000

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -24,3 +24,35 @@ job('NodeJS Docker example') {
         }
     }
 }
+
+// Same with docker push 
+
+job('NodeJS_Docker_Push_DSL') {
+    scm {
+        git('https://github.com/yaqoobch/jenkins-course.git')
+    }
+
+    triggers {
+
+        scm('H/5 * * * *')
+
+    }
+
+    wrappers {
+        nodejs('nodejs')
+
+    }
+
+    steps {
+        dockerBuildAndPublish{
+            repositoryName('yaqoobc/nodejs-docker-demo')
+            tag('${GIT_REVISION,lenght=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+
+    }
+

3c56cdfaf5092f85079d7b48bea7075a3263e689
yaqoobch <47523252+yaqoobch@users.noreply.github.com>
2019-12-20 17:01:57 +0000

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,29 +1,33 @@
-job('NodeJS Docker example') {
+// Same with docker push 
+
+job('NodeJS_Docker_Push_DSL') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-        }
+        git('https://github.com/yaqoobch/jenkins-course.git')
+      //   node / gitConfigName('yaqoobch')
+        //    node / gitConfigEmail('jacob@infopower.co.uk')
     }
+
     triggers {
+
         scm('H/5 * * * *')
+
     }
+
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+        nodejs('nodejs')
     }
+
     steps {
-        dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
+        dockerBuildAndPublish{
+            repositoryName('yaqoobc/nodejs-docker-demo')
+            tag('${GIT_REVISION,lenght=9}')
             registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
             skipDecorate()
         }
+
     }
 }
-
-
-
+    

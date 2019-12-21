778b51214255ee3487a79a4db160debb7336fcc7
Jacob <47523252+yaqoobch@users.noreply.github.com>
2019-12-21 09:11:44 +0000

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -25,4 +25,35 @@ job('NodeJS_By_DSL') {
     }
 }
 
+// Same with docker push 
+
+job('NodeJS_Docker_Push_DSL') {
+    scm {
+         github('yaqoobch/jenkins-course')
+       
+    }
+
+    triggers {
+
+        scm('H/5 * * * *')
+
+    }
+
+    wrappers {
+        nodejs('NodeJS')
+    }
+
+    steps {
+        dockerBuildAndPublish{
+            repositoryName('yaqoobc/nodejs-docker-demo')
+           // tag('${GIT_REVISION,lenght=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+
+    }
+}
 
